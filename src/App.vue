79c79ba9cddd2b777298cbed404a8104f61f2070
YorBeto <jesus.t.morales27@gmail.com>
2024-07-17 18:32:36 -0600

    mi primer commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+</script>
+
+<template>
+    <v-row class="fill-height">
+      <v-col>
+        <v-img
+        height="100vh"
+        aspect-ratio="16/9"
+        cover
+        src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fes.vecteezy.com%2Ffotos-gratis%2Fspiderman&psig=AOvVaw10yLCpRi39aNvhKPpYPgc5&ust=1720557759157000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCIj68qOnmIcDFQAAAAAdAAAAABAK"
+        ></v-img>
+      </v-col>
+      <v-col>
+        <v-card class="text-white bg-black">
+          <v-title class="bg-red">titulo</v-title>
+        </v-card>
+        
+      </v-col>
+    </v-row>
+</template>
